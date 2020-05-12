ffdc5198e49219bd259f1bf3871422107a61eac1
uvsq21918311 <elkadi96hotmail.com>
2020-05-13 00:26:29 +0200

    Add class FormeGroupe

---
+++ src/main/java/fr/uvsq/pglp_9_9/Dessin/FormeGroupe.java
@@ -0,0 +1,27 @@
+package fr.uvsq.pglp_9_9.Dessin;
+
+import java.util.ArrayList;
+
+public class FormeGroupe extends Forme{
+
+	private ArrayList<Forme> formes;
+	
+	public FormeGroupe(String nomVar) {
+		super(nomVar);
+		formes = new ArrayList<Forme>();
+	}
+
+	
+
+	@Override
+	public void deplace(int x, int y) {
+		for (Forme forme : formes) {
+			forme.deplace(x, y);
+		}
+		
+	}
+
+	
+	
+	
+}
