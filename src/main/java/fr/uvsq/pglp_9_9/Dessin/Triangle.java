aa6f6f440e250726c769c6dbb55127b9ac0d8e6f
uvsq21918311 <elkadi96hotmail.com>
2020-05-13 00:21:16 +0200

    Add class Triangle

---
+++ src/main/java/fr/uvsq/pglp_9_9/Dessin/Triangle.java
@@ -0,0 +1,18 @@
+package fr.uvsq.pglp_9_9.Dessin;
+
+public class Triangle extends Forme{
+
+	//pour un triangle on a un ensemble des 3 points triangle
+	private Point[] points;
+	public Triangle(String nomVar) {
+		super(nomVar);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void deplace(int x, int y) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
