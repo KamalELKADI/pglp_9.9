18e3a8d5a64dba2fd609808baf855fc536278618
uvsq21918311 <elkadi96hotmail.com>
2020-05-13 13:17:18 +0200

    Add class AbstractFactoryDao

---
+++ src/main/java/fr/uvsq/pglp_9_9/Dao/AbstractFactoryDao.java
@@ -0,0 +1,12 @@
+package fr.uvsq.pglp_9_9.Dao;
+
+public abstract class AbstractFactoryDao<T> {
+
+	public abstract T create(T object);
+ 
+    public abstract T find(String id);
+    
+    public abstract T update(T object);
+    
+    public abstract void delete(T object);
+}
