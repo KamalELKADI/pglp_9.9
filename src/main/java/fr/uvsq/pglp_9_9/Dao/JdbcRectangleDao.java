94b3ff231d097dfa4558074762364968bb334741
uvsq21918311 <elkadi96hotmail.com>
2020-05-14 13:09:53 +0200

    Add class RectangleDao

---
+++ src/main/java/fr/uvsq/pglp_9_9/Dao/JdbcRectangleDao.java
@@ -0,0 +1,31 @@
+package fr.uvsq.pglp_9_9.Dao;
+
+import fr.uvsq.pglp_9_9.Dessin.Rectangle;
+
+public class JdbcRectangleDao implements Dao<Rectangle>{
+
+	@Override
+	public Rectangle create(Rectangle object) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Rectangle find(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Rectangle update(Rectangle object) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Rectangle object) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
