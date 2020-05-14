4689660daa32551f56c1f0a2318f3c4314d25cac
uvsq21918311 <elkadi96hotmail.com>
2020-05-14 14:38:35 +0200

    Add class RemoveCommand

---
+++ src/main/java/fr/uvsq/pglp_9_9/Command/RemoveCommand.java
@@ -0,0 +1,47 @@
+package fr.uvsq.pglp_9_9.Command;
+
+import java.util.ArrayList;
+
+import fr.uvsq.pglp_9_9.Dao.Dao;
+import fr.uvsq.pglp_9_9.Dao.JdbcDaoFactory;
+import fr.uvsq.pglp_9_9.Dessin.Carre;
+import fr.uvsq.pglp_9_9.Dessin.Cercle;
+import fr.uvsq.pglp_9_9.Dessin.Forme;
+import fr.uvsq.pglp_9_9.Dessin.FormeGroupe;
+import fr.uvsq.pglp_9_9.Dessin.Rectangle;
+import fr.uvsq.pglp_9_9.Dessin.Triangle;
+
+public class RemoveCommand implements Command{
+
+	 private ArrayList<Forme> list;
+	  
+	    public RemoveCommand(final ArrayList<Forme> f) {
+	        list = f;
+	    }
+	    
+	@Override
+	public void execute() {
+		    JdbcDaoFactory factory = new JdbcDaoFactory();
+	        for (Forme forme : list) {
+	            if (forme.getClass() == Cercle.class) {
+	                Dao<Cercle> dao = factory.CreateCercleDao();
+	                dao.delete((Cercle) forme);
+	            } else if (forme.getClass() == Carre.class) {
+	                Dao<Carre> dao = factory.CreateCarreDAO();
+	                dao.delete((Carre) forme);
+	            } else if (forme.getClass() == Rectangle.class) {
+	                Dao<Rectangle> dao = factory.CreateRectangleDao();
+	                dao.delete((Rectangle) forme);
+	            } else if (forme.getClass() == Triangle.class) {
+	                Dao<Triangle> dao = factory.CreateTriangleDAO();
+	                dao.delete((Triangle) forme);
+	            } else {
+	                Dao<FormeGroupe> dao = factory.CreateGroupeDao();
+	                dao.delete((FormeGroupe) forme);
+	            }
+	        }
+	        factory.close();
+		
+	}
+
+}
