7a0e30b2dc91545ee25d9b5e2d4dc923f886f118
uvsq21918311 <elkadi96hotmail.com>
2020-05-14 12:49:03 +0200

    Add class JdbcDaoFactory

---
+++ src/main/java/fr/uvsq/pglp_9_9/Dao/JdbcDaoFactory.java
@@ -0,0 +1,41 @@
+package fr.uvsq.pglp_9_9.Dao;
+
+import fr.uvsq.pglp_9_9.Dessin.Carre;
+import fr.uvsq.pglp_9_9.Dessin.Cercle;
+import fr.uvsq.pglp_9_9.Dessin.FormeGroupe;
+import fr.uvsq.pglp_9_9.Dessin.Rectangle;
+import fr.uvsq.pglp_9_9.Dessin.Triangle;
+
+public class JdbcDaoFactory extends AbstractFactoryDao<T>{
+
+	@Override
+	public Dao<Carre> CreateCarreDAO() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Dao<Triangle> CreateTriangleDAO() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Dao<Rectangle> CreateRectangleDao() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Dao<Cercle> CreateCercleDao() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Dao<FormeGroupe> CreateGroupeDao() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
