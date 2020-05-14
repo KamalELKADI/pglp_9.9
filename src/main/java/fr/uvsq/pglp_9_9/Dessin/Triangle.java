47806d21c1e567235dabc3d0dbddfa1d89983375
uvsq21918311 <elkadi96hotmail.com>
2020-05-15 00:26:49 +0200

    Update class Triangle

--- src/main/java/fr/uvsq/pglp_9_9/Dessin/Triangle.java
+++ src/main/java/fr/uvsq/pglp_9_9/Dessin/Triangle.java
@@ -3,16 +3,43 @@ package fr.uvsq.pglp_9_9.Dessin;
 public class Triangle extends Forme{
 
 	//pour un triangle on a un ensemble des 3 points triangle
-	private Point[] points;
-	public Triangle(String nomVar) {
-		super(nomVar);
-		// TODO Auto-generated constructor stub
-	}
-
-	@Override
-	public void deplace(int x, int y) {
-		// TODO Auto-generated method stub
-		
-	}
+	    private Point[] points;
+	    
+	    //constructeur
+	    public Triangle(String nomVar, Point point1, Point point2, Point point3) {
+	        super(nomVar);
+	        int trois = 3;
+	        points = new Point[trois];
+	        points[0] = point1.clone();
+	        points[1] = point2.clone();
+	        points[2] = point3.clone();
+	    }
+	    
+	    // deplacer les 3 points du triangle
+	    public void deplace(final int x, final int y) {
+	        final int trois = 3;
+	        for (int i = 0; i < trois; i++) {
+	            points[i].deplace(x, y);
+	        }
+	    }
+	    
+	    
+	    //affichage de la forme
+	    public void affiche() {
+	        super.affiche();
+	        System.out.println("Triangle ("
+	                + "position des points = " + points[0] + ", "
+	                + points[1] + ", " + points[2] + ")");
+	    }
+	    
+	    
+	    //getPoint du triangle
+	    public Point getPosition(final int index) {
+	        if (index < 0 || index > 2) {
+	            throw new IndexOutOfBoundsException();
+	        } else {
+	            return points[index].clone();
+	        }
+	    }
 
 }
