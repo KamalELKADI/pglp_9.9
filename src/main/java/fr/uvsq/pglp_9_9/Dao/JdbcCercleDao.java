83c90a6dd319af8d765d08ba64e3dd9649744e34
uvsq21918311 <elkadi96hotmail.com>
2020-05-14 13:08:18 +0200

    Add class CercleDao

---
+++ src/main/java/fr/uvsq/pglp_9_9/Dao/JdbcCercleDao.java
@@ -0,0 +1,31 @@
+package fr.uvsq.pglp_9_9.Dao;
+
+import fr.uvsq.pglp_9_9.Dessin.Cercle;
+
+public class JdbcCercleDao implements Dao<Cercle>{
+
+	@Override
+	public Cercle create(Cercle object) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Cercle find(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Cercle update(Cercle object) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Cercle object) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
