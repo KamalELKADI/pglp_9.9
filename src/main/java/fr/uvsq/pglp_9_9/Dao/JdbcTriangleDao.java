02009b66fd98fe76d9fd8f151edbfefd1e845d59
uvsq21918311 <elkadi96hotmail.com>
2020-05-14 13:07:13 +0200

    Add class TriangleDao

---
+++ src/main/java/fr/uvsq/pglp_9_9/Dao/JdbcTriangleDao.java
@@ -0,0 +1,31 @@
+package fr.uvsq.pglp_9_9.Dao;
+
+import fr.uvsq.pglp_9_9.Dessin.Triangle;
+
+public class JdbcTriangleDao implements Dao<Triangle>{
+
+	@Override
+	public Triangle create(Triangle object) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Triangle find(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Triangle update(Triangle object) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Triangle object) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
